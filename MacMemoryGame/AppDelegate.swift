03ae82fb2604dbd9663bc6cfcb43bdba8551b80c
Janne Käki <janne@awesomeness.pro>
2018-05-15 23:40:55 +0300

    Initial Commit

---
+++ MacMemoryGame/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MacMemoryGame
+//
+//  Created by Janne Käki on 15/05/2018.
+//  Copyright © 2018 Awesomeness Factory Oy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
